af17c0c4e6326c62ac25409068482574e1d25a0d
tejvi-m <tejvimstays@gmail.com>
2020-04-12 11:06:12 +0530

    removed useless files

--- src/DataFrame/add.cl
+++
@@ -1,4 +0,0 @@
-__kernel void square(__global int * data, __global int * data2, __global int * dest, const unsigned int n) {
-    int id = get_global_id(0);
-    if(id < n) dest[id] = data2[id] + data[id];
-}
--- src/client3.cpp
+++ src/client3.cpp
@@ -8,16 +8,17 @@
 #include <ctime>
 #include <cmath>
 #include <iomanip>
+#include <random>
 
 
-
+using namespace std;
 
 int main(){
-    DataFrame x("./src/example.csv");
-    setup();
-
-    auto  y = x.copy();
-    x.printDF();
+    // DataFrame x("./src/example.csv");
+    // setup();
+    //
+    // auto  y = x.copy();
+    // x.printDF();
 
 
 
@@ -26,6 +27,13 @@ int main(){
     // y.printDF();
     // std::cout << "done\n";
 
+    vector<int> y1(9999999 * 2), x1(9999999 * 2);
+    std::generate(y1.begin(), y1.end(), std::rand);
+    std::generate(x1.begin(), x1.end(), std::rand);
+
+    DataFrame x({new SeriesInt(x1)}, {"a"});
+    DataFrame y({new SeriesInt(y1)}, {"a"});
+
     Graph graph;
     graph.addDF(&x);
     graph.addDF(&y);
@@ -33,14 +41,14 @@ int main(){
     std::cout << graph.getGenName(&y) << std::endl;
 
     graph.insertOperation("add", &x, &y);
-    graph.insertOperation("mul", &x, &y);
-    graph.insertOperation("mul", &x, &y);
-    graph.insertOperation("mul", &x, &y);
-    // graph.insertOperation("mul", &y, &x);
-    graph.insertOperation("mul", &x, 3);
-    graph.insertOperation("mul", &x, 3);
-    graph.insertOperation("mul", &x, 3);
-    graph.insertOperation("sub", &x, 3);
+    // graph.insertOperation("mul", &x, &y);
+    // graph.insertOperation("mul", &x, &y);
+    // graph.insertOperation("mul", &x, &y);
+    // // graph.insertOperation("mul", &y, &x);
+    // graph.insertOperation("mul", &x, 3);
+    // graph.insertOperation("mul", &x, 3);
+    // graph.insertOperation("mul", &x, 3);
+    // graph.insertOperation("sub", &x, 3);
 
 
     auto clock1 = clock();
@@ -48,7 +56,7 @@ int main(){
     auto clock2 = clock();
     std::cout << "time: " << (float)(clock2 - clock1)/CLOCKS_PER_SEC << std::endl;
     std::cout << graph.getKernel("int") << std::endl;
-    x.printDF();
-    y.printDF();
+    // x.printDF();
+    // y.printDF();
     return 0;
 }
