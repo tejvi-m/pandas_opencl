980859fe8ba6fcc1f7939aa014c83c3ff2791b23
tejvi-m <tejvimstays@gmail.com>
2020-04-22 12:01:44 +0530

    python bindings for arithmetic operations: addition

--- src/pybindings.cpp
+++ src/pybindings.cpp
@@ -14,11 +14,15 @@
 #include <string>
 #include <boost/python.hpp>
 
+// void add(DataFrame)
+
  BOOST_PYTHON_MODULE(DataFrame)  // Name here must match the name of the final shared library, i.e. mantid.dll or mantid.so
  {
     boost::python::def("setup", &setup);
     boost::python::class_<DataFrame>("DataFrame")
         .def(boost::python::init<std::string>())
         .def("showDF", &DataFrame::printDF)
+        .def("add", (void(DataFrame::*)(DataFrame&))&DataFrame::add)
+        // .def("")
       ;
  }
