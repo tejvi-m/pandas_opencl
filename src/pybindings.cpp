1fc5c30ea19bac089ee37a2abcba3036a731dc7a
tejvi-m <tejvimstays@gmail.com>
2020-04-22 11:14:30 +0530

    preliminary python bindings for the "DataFrame" library

---
+++ src/pybindings.cpp
@@ -0,0 +1,24 @@
+#include "DataFrame/DataFrame.cpp"
+#include "DataFrame/helpers.cpp"
+#include "DataFrame/Series.h"
+#include "DataFrame/SeriesFloat.cpp"
+#include "DataFrame/SeriesInt.cpp"
+#include "DataFrame/SeriesStr.cpp"
+#include "DataFrame/Graph.h"
+#include "DataFrame/Graph.cpp"
+#include <numeric>
+#include <ctime>
+#include <cmath>
+#include <iomanip>
+#include <random>
+#include <string>
+#include <boost/python.hpp>
+
+ BOOST_PYTHON_MODULE(DataFrame)  // Name here must match the name of the final shared library, i.e. mantid.dll or mantid.so
+ {
+    boost::python::def("setup", &setup);
+    boost::python::class_<DataFrame>("DataFrame")
+        .def(boost::python::init<std::string>())
+        .def("showDF", &DataFrame::printDF)
+      ;
+ }
